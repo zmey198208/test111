625f4833368325889b10cf2a20ac0f5e7a193f1d
ZMEY <v.galyantov@gmail.com>
2019-06-24 12:34:49 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log(leftBorderWidht);
+// var leftBorderWidht = 1;
+// let second = 2;
+// const pi = 3.14;
+
+let persone = {
+    name: "John"
+    age: "30"
+    isMarried: false
+}
+console.log(persone.name)
